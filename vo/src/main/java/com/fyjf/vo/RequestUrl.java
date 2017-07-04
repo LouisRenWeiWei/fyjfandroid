6af8f0a6e33adc60c94bc06a5a2a3b4826860c7f
rww_2010 <rww_2010@126.com>
2017-07-04 21:20:28 +0800

    aa

--- vo/src/main/java/com/fyjf/vo/RequestUrl.java
+++ vo/src/main/java/com/fyjf/vo/RequestUrl.java
@@ -3,7 +3,6 @@ package com.fyjf.vo;
 public class RequestUrl {
     //静态文件
 //     public static String file_base ="http://192.168.1.104/file";//家里ip
-    //public static String file_base ="http://192.168.1.101/file";//
     public static String file_base ="http://47.93.217.20:8081/file";//线上
     public static String pdf_file =file_base+"/pdf/";
     public static String pdf_file_upload =file_base+"/upload/";
@@ -13,13 +12,11 @@ public class RequestUrl {
     public static String file_download =file_base+"/upload/";
 
 //     public static String html_base ="http://192.168.1.104/fyjfadmin";//家里ip
-//public static String html_base ="http://192.168.1.101/fyjfadmin";//
     public static String html_base ="http://47.93.217.20:8081/fyjfadmin";//线上
     public static String customer_report_analysis = html_base+"/fyjf/report/echarts.html?customerId=";
 
 
 //     public static String base ="http://192.168.1.104/api/api";//家里ip
-//public static String base ="http://192.168.1.101/api/api";//家里ip
     public static String base ="http://47.93.217.20:8081/fyjf/api";//线上
     public static String login =base+"/login/v2";
     public static String loan_check = base+"/customer/list/by/bank/user";
