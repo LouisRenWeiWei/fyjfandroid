0a6d0a6875aba1cee3b05983b233a16c2025a895
596861134 <chengzhengfu93@163.com>
2017-07-14 19:00:43 +0800

    集成极光推送

--- app/src/main/java/com/fyjf/all/app/BaseApplication.java
+++ app/src/main/java/com/fyjf/all/app/BaseApplication.java
@@ -1,18 +1,26 @@
 package com.fyjf.all.app;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.android.volley.toolbox.Volley;
 import com.antfortune.freeline.FreelineCore;
 import com.liulishuo.filedownloader.FileDownloader;
 
+import cn.jpush.android.api.JPushInterface;
+
 public class BaseApplication extends Application{
+
+	private Context mContext;
 
 	public void onCreate() {
 		super.onCreate();
+		mContext = this;
 		FreelineCore.init(this);
 		AppData.init(this);
 		Volley.init(this,false);
 		FileDownloader.init(this);
+		JPushInterface.init(this);//极光推送
+		JPushInterface.setDebugMode(true);//极光送Log
 	}
 }
